535ba3d325ed1202fe723cbfa5ee62b3894f7324
Lars Ivar Hatledal <laht@ntnu.no>
2018-03-21 14:10:11 +0100

    fixed upside down force sensor

--- snake/snake_module.py
+++ snake/snake_module.py
@@ -151,7 +151,7 @@ class TypeC(ModuleAssembly):
 
         self.upper = UpperPart(app)
         self.intermediate = IntermediatePart(app, material)
-        self.intermediate.setRotation(agx.EulerAngles(0, 0, math.pi))
+        self.intermediate.setRotation(agx.EulerAngles(math.pi, 0, math.pi))
         self.intermediate.setPosition(agx.Vec3(module_len/2 + intermediate_len, 0, 0))
 
         self.add(self.upper)
